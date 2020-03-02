0be3005851570f90ea22471bb4390076ecd2c781
Elisha Kramer <kramer1346@gmail.com>
2020-03-02 19:28:07 +0200

    documenting how to fetch neccessary URL's and documenting how to grab the book data

--- js/app/extras.js
+++ js/app/extras.js
@@ -1,10 +1,28 @@
+//on orders page:
+
+to grab product info:
+document.querySelectorAll('.a-fixed-left-grid-inner')
+
+
+//In order to navigate to the right URL's, really just need one parameter
+//may be worthwhile to fetch the current url right before we start scraping,
+// then append to it search for the "orderFilter" param in the URL
+// If the param is there, replace it, else append to the given URL.
+'https://www.amazon.com/gp/your-account/order-history?orderFilter=year-2019'
+
+To fetch the options available for the given amazon user:
+document.querySelectorAll('#timePeriodForm span select option')
+
+document.querySelector('#timePeriodForm')
+
+
 $state.go('home.fetch-amazon-data');
 
 window.localStorage.getItem('user')
 
 chrome.storage.sync.get(['key'], function(result) {
-          console.log('Value currently is ' + result.key);
-        });
+  console.log('Value currently is ' + result.key);
+});
 
 
 // get creds of client's pinterest users
