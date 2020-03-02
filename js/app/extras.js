ba890a88c5e59ecf6d1c8b26c630b5c04e0bf653
Elisha Kramer <kramer1346@gmail.com>
2020-03-02 21:12:43 +0200

    more notes on how to scrape amazon orders properly

--- js/app/extras.js
+++ js/app/extras.js
@@ -1,7 +1,35 @@
+
+
 //on orders page:
+From All Orders of 2020 Page:'https://www.amazon.com/gp/your-account/order-history?opt=ab&digitalOrders=1&unifiedOrders=1&returnTo=&orderFilter=year-2020'
+
+Step 1: you need to click unto the button: "Order Details" or "See all 7 products"
+
+Step 2: That full page will befor example:"https://www.amazon.com/gp/your-account/order-details/ref=ppx_yo_dt_b_order_details_o00_s00?ie=UTF8&orderID=114-4917364-5138627"
+
+Then, this script works: 
+
+let products = document.querySelectorAll('.a-fixed-left-grid-inner')
+
+for (i = 0; i < products.length; i++) {
+    let item = {};
+    
+    let cleanedUpValues = products[i].innerText.split("\n");
+    item.product_title = cleanedUpValues[0];    
+    item.product_by = cleanedUpValues[1]; 
+    item.product_cost = cleanedUpValues[3];   
+
+    console.log(item);
+}
+
+
 
 to grab product info:
-document.querySelectorAll('.a-fixed-left-grid-inner')
+let products = document.querySelectorAll('.a-fixed-left-grid-inner')
+
+
+in case you need it in the future:
+let currencies = ['د.إ','Af','L','Դ','Kz','$','$','ƒ','ман','КМ','$','৳','лв','ب.د','₣','$','$','Bs.','R$','$','','P','Br','$','$','₣','₣','$','¥','$','₡','$','$','Kč','₣','kr','$','د.ج','£','Nfk','','€','$','£','£','ლ','₵','£','D','₣','Q','$','$','L','Kn','G','Ft','Rp','₪','₹','ع.د','﷼','Kr','$','د.ا','¥','Sh','','៛','₩','₩','د.ك','$','〒','₭','ل.ل','Rs','$','L','ل.د','د.م.','L','','ден','K','₮','P','UM','₨','ރ.','MK','$','RM','MTn','$','₦','C$','kr','₨','$','ر.ع.','B/.','S/.','K','₱','₨','zł','₲','ر.ق','L','din','р.','₣','ر.س','$','₨','£','kr','$','£','Le','Sh','$','Db','ل.س','L','฿','ЅМ','m','د.ت','T$','₤','$','$','Sh','₴','Sh','$','$','','Bs F','₫','Vt','T','₣','$','₣','﷼','R','ZK','$'];
 
 
 //In order to navigate to the right URL's, really just need one parameter
