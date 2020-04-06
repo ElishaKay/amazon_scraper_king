4d836f1ff968548437d26b7fd59ef81409f34b93
Elisha Kramer <kramer1346@gmail.com>
2020-04-06 15:53:34 +0300

    moved css into content page and the div still looks good

--- js/app/content.js
+++ js/app/content.js
@@ -153,6 +153,67 @@ function loadSideBar(config){
         .hidden{
             display: none;
         }
+
+        #thetogglebutton{
+          position: fixed;
+          top: 50px;
+          right: 80px;
+          z-index: 500;
+        }
+
+        .sidebar {
+          height: 100%;
+          width: 400px;
+          position: fixed;
+          top: 0;
+          right: 0;
+          /*background-color: #EF8354;*/
+          z-index: 200;
+        }
+
+        .bar {
+          display: block;
+          height: 5px;
+          width: 50px;
+          background-color: #EF8354;
+          margin: 10px auto;
+        }
+
+        .middle {
+          margin: 0 auto;
+        }
+
+        .bar {
+          -webkit-transition: all .7s ease;
+          -moz-transition: all .7s ease;
+          -ms-transition: all .7s ease;
+          -o-transition: all .7s ease;
+          transition: all .7s ease;
+        }
+
+        .sidebar-list {
+          padding-right: 30px;
+          padding-bottom: 50px;
+          padding-left: 30px;
+          margin: 0;
+          list-style: none;
+          position: absolute;
+          margin-top: 150px;
+        }
+
+        .sidebar.active{
+          background-color: #E5E9EC;
+        }
+
+        .sidebar-list h2{
+          padding-bottom: 30px;
+          text-align: center;
+        }
+
+        .sidebar-list p{
+          padding-left: 25px;
+          font-size: 15px;
+        }
     </style>`;
 
     var sideNav = document.createElement('div');
