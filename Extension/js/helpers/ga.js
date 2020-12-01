61b52def160d91a4f6c940aa09ded02ee579a8ad
Elisha Kramer <kramer1346@gmail.com>
2020-12-01 12:50:09 +0200

    all

--- Extension/js/helpers/ga.js
+++ Extension/js/helpers/ga.js
@@ -24,22 +24,4 @@ _gaq.push(['_trackPageview']);
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(ga, s);
 })();
-/**
- * Track a click on a button using the asynchronous tracking API.
- *
- * See http://code.google.com/apis/analytics/docs/tracking/asyncTracking.html
- * for information on how to use the asynchronous tracking API.
- */
-function trackButtonClick(e) {
-  _gaq.push(['_trackEvent', e.target.id, 'clicked']);
-}
-/**
- * Now set up your event handlers for the popup's `button` elements once the
- * popup's DOM has loaded.
- */
-document.addEventListener('DOMContentLoaded', function () {
-  var buttons = document.querySelectorAll('button');
-  for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', trackButtonClick);
-  }
-});
+
