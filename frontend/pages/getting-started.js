92d07587c8af4a4a5fc4a84afc5cf267cbcb5150
Elisha Kramer <kramer1346@gmail.com>
2020-12-09 16:41:55 +0200

    added meta descriptions and Titles to the Login, Signup, and all other views

--- frontend/pages/getting-started.js
+++ frontend/pages/getting-started.js
@@ -1,12 +1,37 @@
+import Head from 'next/head';
+import { API, DOMAIN, APP_NAME, FB_APP_ID } from '../config';
 import Layout from '../components/Layout';
 import Link from 'next/link';
 import '../components/blog/BlogPage.css';
 import HowItWorks from '../components/tutorials/HowItWorks';
 
 const GettingStarted = () => {
+	const head = () => (
+        <Head>
+            <title>
+                Get Started | {APP_NAME}
+            </title>
+            <meta property="og:title" content="Getting Started | Amazon Product King" />
+            <meta
+                property="og:description"
+                content="Check out what other users purchased, write reviews, comment on other people's posts, and make up to 10% commission when someone purchases an Amazon Product via one of your pages!"
+            />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={`${DOMAIN}`} />
+            <meta property="og:site_name" content={`${APP_NAME}`} />
+
+            <meta property="og:image" content={`${DOMAIN}/static/images/large_rocket.jpeg`} />
+            <meta property="og:image:secure_url" content={`${DOMAIN}/static/images/large_rocket.jpeg`} />
+            <meta property="og:image:type" content="image/jpg" />
+            <meta property="fb:app_id" content={`${FB_APP_ID}`} />
+        </Head>
+    );
+
     return (
         <Layout>
+        	{head()}
             <HowItWorks />
+        	}
         </Layout>
     );
 };
